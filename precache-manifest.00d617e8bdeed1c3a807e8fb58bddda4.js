069e09bed04fb3c151a15edac3e55b8e058a1e49
d871 <dakopf@gmail.com>
2021-02-07 12:13:36 -0800

    first commit

---
+++ precache-manifest.00d617e8bdeed1c3a807e8fb58bddda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4a37abe6d2639c59c3024ec686f2bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96fcb2f8e157fc143761",
+    "url": "/static/css/main.29a73f8c.chunk.css"
+  },
+  {
+    "revision": "cb5b3720069a691f6314",
+    "url": "/static/js/2.f504f50b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f504f50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fcb2f8e157fc143761",
+    "url": "/static/js/main.f725a7b8.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  }
+]);
